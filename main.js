968494f70545c6278ef8195a5748d4e3602f6c44
Stardrake95 <157759631+Stardrake95@users.noreply.github.com>
2024-04-11 17:16:01 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-const description = []
-let track = 0;
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseApp = initializeApp ({
+    apiKey: "AIzaSyBFPDSlVYKiW1mpKCbGYZiN94qF_WPg-9g",
+    authDomain: "info1601-grproject.firebaseapp.com",
+    projectId: "info1601-grproject",
+    storageBucket: "info1601-grproject.appspot.com",
+    messagingSenderId: "48425542087",
+    appId: "1:48425542087:web:6e6dac09cb7da63da4b9e2",
+    measurementId: "G-T83E3K6DDE"
+});
+
+const db = getFirestore(firebaseApp);
 
 async function getUser (user) {
 const url = `https://instagram-scraper-api2.p.rapidapi.com/v1/info?username_or_id_or_url=${user}`;
