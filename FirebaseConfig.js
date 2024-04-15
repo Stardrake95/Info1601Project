7f2e36fc0831966cd0bf7aa045ea191cd36a0193
Stardrake95 <157759631+Stardrake95@users.noreply.github.com>
2024-04-15 16:14:21 -0400

    Create FirebaseConfig.js

---
+++ FirebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+
+const firebaseApp = initializeApp ({
+    apiKey: "AIzaSyBFPDSlVYKiW1mpKCbGYZiN94qF_WPg-9g",
+    authDomain: "info1601-grproject.firebaseapp.com",
+    projectId: "info1601-grproject",
+    storageBucket: "info1601-grproject.appspot.com",
+    messagingSenderId: "48425542087",
+    appId: "1:48425542087:web:6e6dac09cb7da63da4b9e2",
+    measurementId: "G-T83E3K6DDE"
+});
